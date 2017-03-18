5766b2d84ebcb6e060a7fb54d42ad4909627c112
Wesley vieira <wesleyvieira12@gmail.com>
2017-03-18 13:13:22 +0000

    Adicionando usuario no seed e configurando os testes

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ module Petshop
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    #I18N
     config.i18n.default_locale = :pt_BR
+
+    #Rspec configuration ( torna o rspec o arquivo de teste padrão)
+    config.generators do |g|
+    	g.test_framework :rspec, :view_specs => false,
+    							 :controller_specs => false,
+    							 :helper_specs => false,
+    							 :routing_specs => false,
+    							 :request_specs => false
+    end
+
   end
 end
